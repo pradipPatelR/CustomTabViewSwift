fd16ff7baab337e1d462f783654434a184c54e22
Vikram Jagad <>
2021-03-09 16:14:31 +0530

    first commit

---
+++ CustomTabViewSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTabViewSwift
+//
+//  Created by Vikram Jagad on 09/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
